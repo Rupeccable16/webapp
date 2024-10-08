6fba8997f8e297f8fe91ebdd0fba16148b6406b6
RupeshRokade16 <rokade.r@northeastern.edu>
2024-10-07 20:28:52 -0400

    test workflow2

--- Middleware/basicAuth.js
+++ Middleware/basicAuth.js
@@ -18,7 +18,7 @@ exports.authorize = async (req, res, next) => {
 
   if (!user){
     console.log("Unauthorized User Token");
-    return res.status(401).send();
+    return res.status(401).send(); //Unauthorized
   }
   const passAuth = await bcrypt.compareSync(password, user.password);
 
