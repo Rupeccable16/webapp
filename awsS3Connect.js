c95519efda4e26cd4425cdb10c58ec406a72b7f2
RupeshRokade16 <rokade.r@northeastern.edu>
2024-10-31 00:09:39 -0400

    add comment

--- awsS3Connect.js
+++ awsS3Connect.js
@@ -16,6 +16,7 @@ const region = process.env.AWS_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
+//Create client before using
 const s3 = new S3Client({
   region: region,
 });
